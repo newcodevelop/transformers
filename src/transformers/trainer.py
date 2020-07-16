3c9092cedc0ecfa7a0026c7f13419a127de9cf62
Dibyanayan Bandyopadhyay <dibyanayan@gmail.com>
2020-07-17 01:34:45 +0530

    Update trainer.py

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -410,7 +410,7 @@ class Trainer:
             if self.args.n_gpu > 1:
                 inputs["return_tuple"] = True
 
-            output = model(**inputs).view(1, -1)
+            output = model(**inputs)[0].view(1, -1)
             logger.info(f"output = {output}")
             #output = self.model(input).view(1, -1)
             label = output.max(1)[1].view(-1)
